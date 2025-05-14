64e27bd395e598c0d22773c1837408eef2635a8c
Martin Žajdlík <martinzajdlik@Martin--MacBook-Air.local>
2025-05-14 21:28:21 +0200

    create recappy repo

---
+++ src/main/java/cz/martinzajdlik/recappy_book/RecappyBookApplication.java
@@ -0,0 +1,13 @@
+package cz.martinzajdlik.recappy_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecappyBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecappyBookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/martinzajdlik/recappy_book/RecappyBookApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.martinzajdlik.recappy_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecappyBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
