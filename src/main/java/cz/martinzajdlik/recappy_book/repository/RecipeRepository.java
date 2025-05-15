8ddba0c31cf2ab7b6ddc47e1c21790a764f28f57
Martin Žajdlík <martinzajdlik@Martin--MacBook-Air.local>
2025-05-15 18:07:16 +0200

    create interface RecipeRepository

---
+++ src/main/java/cz/martinzajdlik/recappy_book/repository/RecipeRepository.java
@@ -0,0 +1,9 @@
+package cz.martinzajdlik.recappy_book.repository;
+
+import cz.martinzajdlik.recappy_book.model.Recipe;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RecipeRepository extends JpaRepository<Recipe, Long> {
+}
