42c33f097cc7bcebb52d52d24c8389fb1971941e
Martin Žajdlík <m.zajdlik@seznam.cz>
2025-05-31 20:57:27 +0200

    add localhost:3000

--- src/main/java/cz/martinzajdlik/recappy_book/config/WebConfig.java
+++ src/main/java/cz/martinzajdlik/recappy_book/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("http://localhost:8080") // přidej další adresy pokud budeš potřebovat
+                        .allowedOriginPatterns("http://localhost:3000")// přidej další adresy pokud budeš potřebovat
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
